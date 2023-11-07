37b46a448d6a45c3325b3bbf775f22739dae52b2
Bruno Wilson <brunowilson4@gmail.com>
2023-11-06 23:46:34 -0300

    Adiciona forma de compartilhar dataset convertido para facilitar

--- app/src/main/java/com/example/createmesh/model/CustomFaceMesh.kt
+++ app/src/main/java/com/example/createmesh/model/CustomFaceMesh.kt
@@ -3,7 +3,7 @@ package com.example.createmesh.model
 import com.google.mlkit.vision.facemesh.FaceMesh
 import java.io.Serializable
 
-class CustomFaceMesh    (fileName: String, faceMesh: FaceMesh, isFraud: Boolean) {
+class CustomFaceMesh(fileName: String, faceMesh: FaceMesh, isFraud: Boolean) {
     val fileName: String
     val allPoints: MutableMap<Int, Point> = mutableMapOf()
     val isFraud: Boolean
